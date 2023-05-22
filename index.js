b91af8bdf0807a7d73142444b0a23f481cb12b34
Zunnurain badar <zunnurainbadar@gmail.com>
2023-05-22 08:22:31 +0500

    Added Sum API

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const bodyParser  = require('body-parser')
 const app = express()
-const port = 3000
+const port = 80
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
+}
+
+app.post('/add', (req, res) => {
+    if (!req.body.num1) {
+        return res.status(404).send({ code: 404, message: "Number 1 Not found" })
+    }
+    if (!req.body.num2) {
+        return res.status(404).send({ code: 404, message: "Number 2 Not found" })
+    }
+    if(!containsOnlyNumbers(req.body.num1)){
+        return res.status(400).send({ code: 400, message: "Number 1 is not an integer" })
+    }
+    if(!containsOnlyNumbers(req.body.num2)){
+        return res.status(400).send({ code: 400, message: "Number 2 is not an integer" })
+    }
+    const body = req.body;
+    const sum = parseInt(body.num1) + parseInt(body.num2);
+    return res.status(200).send({ message: "Success", data: { sum: sum } })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
